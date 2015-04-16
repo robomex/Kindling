576562d23f4a45d62ac8b525bcc3a4f715eda07a
robomex <atmorales@gmail.com>
2015-04-15 23:50:47 -0500

    initial commit

---
+++ Kindling/CardView.swift
@@ -0,0 +1,14 @@
+//
+//  CardView.swift
+//  Kindling
+//
+//  Created by Tony Morales on 4/15/15.
+//  Copyright (c) 2015 Tony Morales. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView {
+    
+}
