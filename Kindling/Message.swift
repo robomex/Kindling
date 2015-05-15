e8d69d5972716e39fe240442eb036b0db2b1f2b8
robomex <atmorales@gmail.com>
2015-05-14 23:23:26 -0500

    create message struct/data type

---
+++ Kindling/Message.swift
@@ -0,0 +1,52 @@
+//
+//  Message.swift
+//  Kindling
+//
+//  Created by Tony Morales on 5/14/15.
+//  Copyright (c) 2015 Tony Morales. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let message: String
+    let senderID: String
+    let date: NSDate
+}
+
+private let ref = Firebase(url: "https://kindling-atm.firebaseio.com/messages")
+private let dateFormat = "yyyyMMddHHmmss"
+
+private func dateFormatter() -> NSDateFormatter {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = dateFormat
+    return dateFormatter
+}
+
+func saveMessage(matchID: String, message: Message) {
+    ref.childByAppendingPath(matchID).updateChildValues([dateFormatter().stringFromDate(message.date) : ["message" : message.message, "sender" : message.senderID]])
+}
+
+private func snapshotToMessage(snapshot: FDataSnapshot) -> Message {
+    
+    let date = dateFormatter().dateFromString(snapshot.key)
+    
+    let sender = snapshot.value["sender"] as? String
+    let text = snapshot.value["message"] as? String
+    return Message(message: text!, senderID: sender!, date: date!)
+}
+
+func fetchMessages(matchID: String, callback: ([Message]) -> ()) {
+    
+    ref.childByAppendingPath(matchID).queryLimitedToFirst(25).observeSingleEventOfType(FEventType.Value, withBlock: {
+        snapshot in
+        
+        var messages = Array<Message>()
+        let enumerator = snapshot.children
+        
+        while let data = enumerator.nextObject() as? FDataSnapshot {
+            messages.append(snapshotToMessage(data))
+        }
+        callback(messages)
+    })
+}
